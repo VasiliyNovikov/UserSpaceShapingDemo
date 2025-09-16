d9fdbb794b580f4a314a5efb482c57999efdc3e5
Vasyl Novikov <vanoviko@microsoft.com>
2025-09-16 18:14:24 +0000

    UMEM tests

---
+++ UserSpaceShapingDemo.Tests/UMemoryTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using UserSpaceShapingDemo.Lib;
+
+namespace UserSpaceShapingDemo.Tests;
+
+[TestClass]
+public sealed class UMemoryTests
+{
+    [TestMethod]
+    public void UMemory_Create_Delete()
+    {
+        using var fillRing = new ProducerRingBuffer();
+        using var completionRing = new ConsumerRingBuffer();
+        using var umem = new UMemory(fillRing, completionRing, 4096);
+    }
+}
