6121331432f24d2a9f14406ee1f31a4654e4e6d8
Vasyl Novikov <vanoviko@microsoft.com>
2025-09-16 18:27:23 +0000

    UMEM tests

--- UserSpaceShapingDemo.Tests/UMemoryTests.cs
+++ UserSpaceShapingDemo.Tests/UMemoryTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.ComponentModel;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using UserSpaceShapingDemo.Lib;
 
@@ -13,5 +15,14 @@ public sealed class UMemoryTests
         using var fillRing = new ProducerRingBuffer();
         using var completionRing = new ConsumerRingBuffer();
         using var umem = new UMemory(fillRing, completionRing, 4096);
+    }
+
+    [TestMethod]
+    public void UMemory_Create_Argument_Error()
+    {
+        using var fillRing = new ProducerRingBuffer();
+        using var completionRing = new ConsumerRingBuffer();
+        var exception = Assert.ThrowsExactly<Win32Exception>(() => new UMemory(fillRing, completionRing, 0));
+        Assert.AreEqual(22, exception.NativeErrorCode);
     }
 }
