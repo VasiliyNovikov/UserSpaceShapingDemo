d7471b91f375da3b03e7d5b818d313ba5e83f891
Vasyl Novikov <vanoviko@microsoft.com>
2025-09-18 15:18:02 +0000

    NlAddress

--- UserSpaceShapingDemo.Lib/Interop/LibNl3.cs
+++ UserSpaceShapingDemo.Lib/Interop/LibNl3.cs
@@ -20,14 +20,33 @@ internal static unsafe partial class LibNl3
     [LibraryImport(Lib, EntryPoint = "nl_socket_alloc", SetLastError = true)]
     public static partial nl_sock* nl_socket_alloc();
 
-    // void nl_socket_free(struct nl_sock *);
+    // void nl_socket_free(struct nl_sock *)
     [LibraryImport(Lib, EntryPoint = "nl_socket_free")]
     public static partial void nl_socket_free(nl_sock* sock);
 
-    // int nl_connect(struct nl_sock *, int);
+    // int nl_connect(struct nl_sock *, int)
     [LibraryImport(Lib, EntryPoint = "nl_connect")]
     public static partial int nl_connect(nl_sock* sock, int protocol);
 
+    // int nl_addr_parse(const char *addrstr, int hint, struct nl_addr **result)
+    [LibraryImport(Lib, EntryPoint = "nl_addr_parse", StringMarshalling = StringMarshalling.Utf8)]
+    public static partial int nl_addr_parse(string addrstr, int hint, out nl_addr* result);
+
+    // struct nl_addr *nl_addr_build(int family, void *buf, size_t size)
+    [LibraryImport(Lib, EntryPoint = "nl_addr_build", SetLastError = true)]
+    public static partial nl_addr* nl_addr_build(int family, void* buf, nuint size);
+    
+    // char *nl_addr2str(struct nl_addr *addr, char *buf, size_t size)
+    [LibraryImport(Lib, EntryPoint = "nl_addr2str")]
+    public static partial byte* nl_addr2str(nl_addr* addr, byte* buf, nuint size);
+    
+    // void nl_addr_put(struct nl_addr *addr)
+    [LibraryImport(Lib, EntryPoint = "nl_addr_put")]
+    public static partial void nl_addr_put(nl_addr* addr);
+
     [StructLayout(LayoutKind.Sequential)]
     public struct nl_sock;
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct nl_addr;
 }
---
+++ UserSpaceShapingDemo.Lib/Nl3/NlAddress.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices.Marshalling;
+
+using UserSpaceShapingDemo.Lib.Interop;
+
+namespace UserSpaceShapingDemo.Lib.Nl3;
+
+public sealed unsafe class NlAddress : CriticalFinalizerObject, IDisposable
+{
+    private const int MaxAddressStringLength = 128;
+
+    internal LibNl3.nl_addr* Addr { get; }
+
+    internal NlAddress(LibNl3.nl_addr* addr)
+    {
+        Addr = addr == null ? throw new ArgumentNullException(nameof(addr)) : addr;
+    }
+
+    [SkipLocalsInit]
+    public NlAddress(IPAddress address)
+    {
+        ArgumentNullException.ThrowIfNull(address);
+
+        Span<byte> addressBytes = stackalloc byte[16];
+        address.TryWriteBytes(addressBytes, out int size);
+        fixed (byte* p = addressBytes)
+        {
+            var addr = LibNl3.nl_addr_build((int)address.AddressFamily, p, (nuint)size);
+            if (addr == null)
+                throw NlException.FromLastPInvokeError();
+            Addr = addr;
+        }
+    }
+
+    private void ReleaseUnmanagedResources()
+    {
+        if (Addr is not null)
+            LibNl3.nl_addr_put(Addr);
+    }
+
+    ~NlAddress() => ReleaseUnmanagedResources();
+
+    public void Dispose()
+    {
+        ReleaseUnmanagedResources();
+        GC.SuppressFinalize(this);
+    }
+
+    [SkipLocalsInit]
+    public override string ToString()
+    {
+        var addressBuffer = stackalloc byte[MaxAddressStringLength];
+        var str = LibNl3.nl_addr2str(Addr, addressBuffer, MaxAddressStringLength);
+        return str is null
+            ? throw NlException.FromLastPInvokeError()
+            : Utf8StringMarshaller.ConvertToManaged(str)!;
+    }
+}
