76e04de7bc54a5e987d5e745b78f3cef00248308
Vasyl Novikov <vanoviko@microsoft.com>
2025-09-18 12:33:42 +0000

    VEth test improved

--- UserSpaceShapingDemo.Tests/RtnlVEthLinkTests.cs
+++ UserSpaceShapingDemo.Tests/RtnlVEthLinkTests.cs
@@ -10,11 +10,27 @@ public sealed class RtnlVEthLinkTests
     [TestMethod]
     public void RtnlVEthLink_Alloc_Add_Delete()
     {
+        const string vethName = "test_veth0";
+        const string vethPeerName = "test_veth1";
+
         using var socket = new RtnlSocket();
         using var veth = RtnlVEthLink.Allocate();
-        veth.Name = "test_veth0";
-        veth.Peer.Name = "test_veth1";
+        veth.Name = vethName;
+        veth.Peer.Name = vethPeerName;
         socket.Add(veth);
-        socket.Delete(veth);
+        try
+        {
+            Script.Exec("ip", "link", "show", vethName);
+            Script.Exec("ip", "link", "show", vethPeerName);
+
+            socket.Delete(veth);
+
+            Assert.ThrowsExactly<AssertFailedException>(() => Script.Exec("ip", "link", "show", vethName));
+            Assert.ThrowsExactly<AssertFailedException>(() => Script.Exec("ip", "link", "show", vethPeerName));
+        }
+        finally
+        {
+            Script.ExecNoThrow("ip", "link", "del", vethName);
+        }
     }
 }
