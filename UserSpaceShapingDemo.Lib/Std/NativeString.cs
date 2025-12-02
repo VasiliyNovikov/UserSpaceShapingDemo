0ba870acd6631e1316b0dac5c34107608ee6ffca
Vasyl Novikov <vanoviko@microsoft.com>
2025-12-02 13:56:58 +0000

    Fix

--- UserSpaceShapingDemo.Lib/Std/NativeString.cs
+++ UserSpaceShapingDemo.Lib/Std/NativeString.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.Marshalling;
 
@@ -9,31 +8,21 @@ namespace UserSpaceShapingDemo.Lib.Std;
 
 public static class NativeString
 {
-    public static unsafe string Format(byte* format, void* ap, int initialBufferSize = 256)
-    {
-        var bufferSize = initialBufferSize;
-        string? result;
-        while (!TryFormat(format, ap, bufferSize, out result))
-            bufferSize *= 2;
-        return result;
-    }
-
     [SkipLocalsInit]
-    private static unsafe bool TryFormat(byte* format, void* ap, int bufferSize, [MaybeNullWhen(false)] out string result)
+    public static unsafe string Format(byte* format, void* ap)
     {
+        var bufferSize = LibC.vsnprintf(null, 0, format, ap);
+
+        if (bufferSize < 0)
+            throw new FormatException("String formatting failed.");
+
+        if (bufferSize == 0)
+            return string.Empty;
+
         Span<byte> buffer = stackalloc byte[bufferSize];
         var bufferPtr = (byte*)Unsafe.AsPointer(ref buffer[0]);
-        var written = LibC.vsnprintf(bufferPtr, (UIntPtr)bufferSize, format, ap);
-        if (written >= bufferSize)
-        {
-            result = null;
-            return false;
-        }
-
-        if (written < 0)
-            throw new InvalidOperationException("String formatting failed.");
-
-        result = Utf8StringMarshaller.ConvertToManaged(bufferPtr)!;
-        return true;
+        return LibC.vsnprintf(bufferPtr, (UIntPtr)bufferSize, format, ap) < bufferSize
+            ? Utf8StringMarshaller.ConvertToManaged(bufferPtr)!
+            : throw new FormatException("String formatting failed: buffer too small.");
     }
 }
