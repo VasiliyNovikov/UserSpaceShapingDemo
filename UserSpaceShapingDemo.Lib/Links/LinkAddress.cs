35955fb94c73e52a84999829065a06a58f1b9928
Vasyl Novikov <vanoviko@microsoft.com>
2025-12-05 15:33:19 +0000

    Fixes

--- UserSpaceShapingDemo.Lib/Links/LinkAddress.cs
+++ UserSpaceShapingDemo.Lib/Links/LinkAddress.cs
@@ -7,8 +7,6 @@ namespace UserSpaceShapingDemo.Lib.Links;
 public readonly struct LinkAddress<TAddress>(TAddress address, byte prefixLength)
     where TAddress : unmanaged, IIPAddress<TAddress>
 {
-    public static AddressFamily Family => TAddress.Version == 4 ? AddressFamily.InterNetwork : AddressFamily.InterNetworkV6;
-
     public TAddress Address => address;
     public byte PrefixLength => prefixLength;
 }
--- UserSpaceShapingDemo.Lib/Links/LinkAddressCollection.cs
+++ UserSpaceShapingDemo.Lib/Links/LinkAddressCollection.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using System.Runtime.InteropServices;
 
 using NetworkingPrimitivesCore;
@@ -12,6 +13,8 @@ namespace UserSpaceShapingDemo.Lib.Links;
 public sealed class LinkAddressCollection<TAddress> : IEnumerable<LinkAddress<TAddress>>
     where TAddress : unmanaged, IIPAddress<TAddress>
 {
+    private static AddressFamily Family => TAddress.Version == 4 ? AddressFamily.InterNetwork : AddressFamily.InterNetworkV6;
+
     private readonly RtnlSocket _socket;
     private readonly int _linkIndex;
 
@@ -24,7 +27,7 @@ public sealed class LinkAddressCollection<TAddress> : IEnumerable<LinkAddress<TA
     public void Add(LinkAddress<TAddress> address)
     {
         using var linkAddr = RtnlAddress.Alloc();
-        using var addr = new NlAddress(address.Address.Bytes, LinkAddress<TAddress>.Family);
+        using var addr = new NlAddress(address.Address.Bytes, Family);
         linkAddr.IfIndex = _linkIndex;
         linkAddr.Address = addr;
         _socket.AddAddress(linkAddr);
@@ -33,7 +36,7 @@ public sealed class LinkAddressCollection<TAddress> : IEnumerable<LinkAddress<TA
     public void Remove(LinkAddress<TAddress> address)
     {
         using var linkAddr = RtnlAddress.Alloc();
-        using var addr = new NlAddress(address.Address.Bytes, LinkAddress<TAddress>.Family);
+        using var addr = new NlAddress(address.Address.Bytes, Family);
         addr.PrefixLength = address.PrefixLength;
         linkAddr.IfIndex = _linkIndex;
         linkAddr.Address = addr;
@@ -48,8 +51,9 @@ public sealed class LinkAddressCollection<TAddress> : IEnumerable<LinkAddress<TA
 
     public IEnumerator<LinkAddress<TAddress>> GetEnumerator()
     {
-        foreach (var rtnlAddress in _socket.GetAddresses())
-            if (rtnlAddress.IfIndex == _linkIndex && rtnlAddress.Address is { } nlAddress && nlAddress.Family == LinkAddress<TAddress>.Family)
+        using var rtnlAddresses = _socket.GetAddresses();
+        foreach (var rtnlAddress in rtnlAddresses)
+            if (rtnlAddress.IfIndex == _linkIndex && rtnlAddress.Address is { } nlAddress && nlAddress.Family == Family)
                 yield return new LinkAddress<TAddress>(MemoryMarshal.Read<TAddress>(nlAddress.Bytes), nlAddress.PrefixLength);
     }
 
