042b2e3c5d5a1d4e9382c8e2333636823385cca2
Vasyl Novikov <vanoviko@microsoft.com>
2025-12-05 17:04:20 +0000

    TEsts

--- UserSpaceShapingDemo.Tests/LinkTests.cs
+++ UserSpaceShapingDemo.Tests/LinkTests.cs
@@ -5,6 +5,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NetworkingPrimitivesCore;
 
 using UserSpaceShapingDemo.Lib.Links;
+using UserSpaceShapingDemo.Lib.Nl3.Route;
 
 namespace UserSpaceShapingDemo.Tests;
 
@@ -47,5 +48,50 @@ public class LinkTests
         var lo = links.FirstOrDefault(l => l.Name == "lo");
         Assert.IsNotNull(lo);
         Assert.IsGreaterThan(0, lo.Index);
+    }
+
+    [TestMethod]
+    public void BridgeLink_Create_Delete()
+    {
+        const string bridgeName = "test_br0";
+        const string bridgeAddress = "10.0.10.1";
+        const byte bridgeAddressPrefix = 30;
+
+        using var collection = new LinkCollection();
+
+        Assert.ThrowsExactly<AssertFailedException>(() => Script.Exec("ip", "link", "show", bridgeName));
+
+        var bridge = collection.CreateBridge(bridgeName);
+        try
+        {
+            var linkInfo = Script.Exec("ip", "link", "show", bridgeName);
+            Assert.AreNotEqual("", linkInfo);
+            Assert.Contains(bridgeName, linkInfo);
+            Assert.Contains("DOWN", linkInfo);
+
+            Assert.IsGreaterThan(0, bridge.Index);
+            Assert.AreEqual(bridgeName, bridge.Name);
+            Assert.IsFalse(bridge.Up);
+            Assert.AreEqual(RtnlBridgePortState.Disabled, bridge.PortState);
+
+            bridge.IPv4Addresses.Add(new LinkAddress<IPv4Address>(IPv4Address.Parse(bridgeAddress), bridgeAddressPrefix));
+
+            Assert.Contains($"{bridgeAddress}/{bridgeAddressPrefix}", Script.Exec("ip", "address", "show", bridgeName));
+
+            bridge.Up = true;
+
+            linkInfo = Script.Exec("ip", "link", "show", bridgeName);
+            Assert.Contains("UP", linkInfo);
+
+            Assert.AreEqual(RtnlBridgePortState.Disabled, bridge.PortState);
+
+            collection.Delete(bridge);
+
+            Assert.ThrowsExactly<AssertFailedException>(() => Script.Exec("ip", "link", "show", bridgeName));
+        }
+        finally
+        {
+            Script.ExecNoThrow("ip", "link", "del", bridgeName);
+        }
     }
 }
