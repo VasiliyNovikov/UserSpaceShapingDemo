cf2ed867ce6ad16f3787af086e0cdf4361730e87
Vasyl Novikov <vanoviko@microsoft.com>
2025-12-15 22:58:32 +0000

    Fix

--- UserSpaceShapingDemo.Lib/Std/NativeEventBase.cs
+++ UserSpaceShapingDemo.Lib/Std/NativeEventBase.cs
@@ -8,12 +8,12 @@ namespace UserSpaceShapingDemo.Lib.Std;
 public abstract unsafe class NativeEventBase(uint initialValue, int flags)
     : FileObject(LibC.eventfd(initialValue, flags).ThrowIfError())
 {
-    private static readonly long* Value = (long*)NativeMemory.Alloc(sizeof(ulong));
-
-    static NativeEventBase() => *Value = 1;
-
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    protected void WriteOne() => Write(Value, sizeof(ulong));
+    protected void WriteOne()
+    {
+        var value = 1ul;
+        Write(&value, sizeof(ulong));
+    }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     protected void Read()
