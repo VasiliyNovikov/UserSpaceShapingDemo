6f12ca88505f6bf0c1f105cc3fda2388aad03b9d
Vasyl Novikov <vanoviko@microsoft.com>
2025-12-15 14:22:07 +0000

    Parallel forwarding testing

--- UserSpaceShapingDemo.Lib/Forwarding/ParallelForwarder.cs
+++ UserSpaceShapingDemo.Lib/Forwarding/ParallelForwarder.cs
@@ -11,10 +11,12 @@ public sealed class ParallelForwarder : IDisposable
     public ParallelForwarder(string ifName1, string ifName2, ForwardingMode mode = ForwardingMode.Generic, uint ifQueueCount = 1)
     {
         _channel = new ForwardingChannel(ifName1, ifName2, mode);
+        var shared = false;
         for (var i = 0u; i < ifQueueCount; ++i)
         {
-            _forwarders.Add(new PipeForwarder(_channel, _channel.Pipe1, i));
-            _forwarders.Add(new PipeForwarder(_channel, _channel.Pipe2, i));
+            _forwarders.Add(new PipeForwarder(_channel, _channel.Pipe1, i, shared));
+            shared = true;
+            _forwarders.Add(new PipeForwarder(_channel, _channel.Pipe2, i, shared));
         }
     }
 
